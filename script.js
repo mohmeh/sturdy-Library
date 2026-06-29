edc0a005239363f97f79429b5f32e97f2a49e777
Adeyemi Olowofoyeku <117146096+mohmeh@users.noreply.github.com>
2026-06-30 00:17:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 const library = [];
+
+function CreateBook (author, title, pages){ 
+  var book = {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  const bookId = crypto.randomUUID(this.book);
+};
+ console.log(book);
+  
+library.push(CreateBook); 
+};
+
+function displayLibrary (){
+for (const book of library){
+  console.log(library);
+}
+  displayLibrary();
+}
+function onClick(){
+  return CreateBook;
+},
+var newBook = document.createElement('button');
+newBook.innerText = "Add new Book";
+newBook.addEventListener('click', onClick);
+document.body.appendChild(newbook);
