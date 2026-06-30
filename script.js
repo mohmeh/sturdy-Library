1840f5f91e625b34f04e06ec055beca9fddbb268
Adeyemi Olowofoyeku <117146096+mohmeh@users.noreply.github.com>
2026-06-30 09:55:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const library = [];
 
 function CreateBook (author, title, pages){ 
-  var book = {
   this.author = author;
   this.title = title;
   this.pages = pages;
-  const bookId = crypto.randomUUID(this.book);
-};
- console.log(book);
-  
-library.push(CreateBook); 
+  this.id = crypto.randomUUID();
+}
+
+let book = new CreateBook ('Moh', 'Bug bounty Js', '541'){
+addBookToLibrary:function (){
+library.push(this.book)};
 };
 
 function displayLibrary (){
@@ -17,7 +17,7 @@ for (const book of library){
   console.log(library);
 }
   displayLibrary();
-}
+};
 function onClick(){
   return CreateBook;
 };
@@ -25,3 +25,19 @@ var newBook = document.createElement('button');
 newBook.innerText = "Add new Book";
 newBook.addEventListener('click', onClick);
 document.body.appendChild(newbook);
+
+function deleteBook(bookId){
+  delete book.bookId;
+}
+var remover = document.createElement('button');
+remover.innerText = "Remove book";
+remover.addEventListener('click', deleteBook);
+document.body.appendChild(remover);
+
+function status (){
+  Createbook.prototype.thisstatus = true;
+}
+var readStatus = document.createElement('button');
+readStatus.innerText = "Remove book";
+readStatus.addEventListener('click', status);
+readStatus.body.appendChild(readStatus);
